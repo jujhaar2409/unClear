5b5886a0e3e02349e637be60576089967d4e725c
jujhaar2409 <jujhaar.singh.123@gmail.com>
2021-04-11 22:08:28 +0530

    assignment 3 complete

---
+++ assignments/a3/live.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(1)
+
+while True:
+    ret, img = cap.read()
+    img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    cv2.imshow('original', img)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('gray', gray)
+
+    blurred = cv2.GaussianBlur(255 - gray, ksize=(41, 41),
+                               sigmaX=10, sigmaY=10)
+    cv2.imshow('blurred', blurred)
+
+    def dodgeV2(image, mask):
+        return cv2.divide(image, 255 - mask, scale=256)
+
+    def burnV2(image, mask):
+        val = 255 - cv2.divide(255 - image, 255 - mask, scale=256)
+        return val
+
+    final = dodgeV2(gray, blurred)
+
+    cv2.imshow('final', final)
+    cv2.imshow('original', img)
+
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+cv2.waitKey(1)
--- assignments/a3/main.py
+++ assignments/a3/main.py
@@ -2,28 +2,44 @@ import cv2
 import numpy as np
 
 img = cv2.imread("img.jpg", 1)
-img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+# img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 
-cv2.imshow('grey scale', img)
+cv2.imshow('original', img)
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('gray', gray)
 
-steps = 20
-mid = 150
+blurred = cv2.GaussianBlur(255 - gray, ksize=(41, 41),
+                            sigmaX=10, sigmaY=10)
+cv2.imshow('blurred', blurred)
 
-for i in range(1, steps + 1):
-    black_upper = np.array([255, 255, mid * i / steps])
-    black_lower = np.array([0, 0, mid * (i - 1) / steps])
-    black_mask = cv2.inRange(hsv, black_lower, black_upper)
-    if i > steps // 3:
-        img[black_mask > 0] = [mid * (i) / steps] * 3
-    else:
-        img[black_mask > 0] = [0] * 3
+def dodgeV2(image, mask):
+    return cv2.divide(image, 255-mask, scale=256)
 
-white_upper = np.array([255, 255, 255])
-white_lower = np.array([0, 0, mid])
-white_mask = cv2.inRange(hsv, white_lower, white_upper)
+final = dodgeV2(gray, blurred)
 
-img[white_mask > 0] = [255, 255, 255]
+# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#
+# steps = 20
+# mid = 150
+#
+# for i in range(1, steps + 1):
+#     black_upper = np.array([255, 255, mid * i / steps])
+#     black_lower = np.array([0, 0, mid * (i - 1) / steps])
+#     black_mask = cv2.inRange(hsv, black_lower, black_upper)
+#     if i > steps // 3:
+#         img[black_mask > 0] = [mid * (i) / steps] * 3
+#     else:
+#         img[black_mask > 0] = [0] * 3
+#
+# white_upper = np.array([255, 255, 255])
+# white_lower = np.array([0, 0, mid])
+# white_mask = cv2.inRange(hsv, white_lower, white_upper)
+#
+# img[white_mask > 0] = [255, 255, 255]
 
-cv2.imshow('black and white', img)
+cv2.imshow('sketch', final)
+cv2.imwrite('sketch.jpg', final)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
